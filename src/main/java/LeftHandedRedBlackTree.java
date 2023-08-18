d1b57d6c5392a7a9f48e1a27c35e18dd0620b0a1
Roman Annenkov <roman.annenkov@gmail.com>
2023-08-18 17:41:11 +0300

    added toString for class tree

--- src/main/java/LeftHandedRedBlackTree.java
+++ src/main/java/LeftHandedRedBlackTree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeftHandedRedBlackTree<V extends Comparable<V>> {
     Node<V> root;
     int size;
@@ -7,18 +10,56 @@ public class LeftHandedRedBlackTree<V extends Comparable<V>> {
         if (root == null) {
             newNode.color = Colors.BLACK;
             root = newNode;
-        } else if ((root.value.compareTo(newNode.value) < 0) && (root.leftChild == null)) {
+            size++;
+        }
+
+        if ((root.value.compareTo(newNode.value) > 0) && (root.leftChild == null)) {
             root.leftChild = newNode;
+            size++;
+        }
+
+        if ((root.value.compareTo(newNode.value) < 0) && (root.rightChild == null)) {
+            root.rightChild = newNode;
+            size++;
         }
     }
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
-        Node<V> currentElement = root;
-        builder.append(currentElement.value).append(" - ").append(currentElement.color);
-        return builder.toString();
+        StringBuilder result = new StringBuilder();
+        List<List<Node<V>>> levelNodes = new ArrayList<>();
+        levelNodes.add(new ArrayList<>(List.of(root)));
+        int levels = size / 2;
+
+        for (int i = 0; i < levels; i++) {
+            List<Node<V>> nextLevelNodes = new ArrayList<>();
+            for (Node<V> node : levelNodes.get(i)) {
+                nextLevelNodes.addAll(node.getChildren());
+            }
+            levelNodes.add(nextLevelNodes);
+        }
+
+        for (int i = 0; i < levelNodes.size(); i++) {
+            for (int j = 0; j < levelNodes.get(i).size(); j++) {
+                for (int k = 0; k < levels - i; k++) {
+                    result.append("\t");
+                }
+                Node<V> currentNode = levelNodes.get(i).get(j);
+                result.append(currentNode.value)
+//                        .append("-")
+//                        .append(currentNode.color)
+                        .append("\t\t");
+            }
+            result.append("\n");
+        }
+
+        return result.toString();
     }
+
+    public int size() {
+        return size;
+    }
+
 
     private static class Node<V> {
         V value;
@@ -31,6 +72,17 @@ public class LeftHandedRedBlackTree<V extends Comparable<V>> {
             this.color = color;
             this.leftChild = leftChild;
             this.rightChild = rightChild;
+        }
+
+        private List<Node<V>> getChildren() {
+            List<Node<V>> result = new ArrayList<>();
+            if (this.leftChild != null) {
+                result.add(this.leftChild);
+            }
+            if (this.rightChild != null) {
+                result.add(this.rightChild);
+            }
+            return result;
         }
 
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         LeftHandedRedBlackTree<Integer> integerTree = new LeftHandedRedBlackTree<>();
         integerTree.add(4);
+        integerTree.add(5);
+        integerTree.add(3);
         System.out.println(integerTree);
 
         LeftHandedRedBlackTree<String> stringTree = new LeftHandedRedBlackTree<>();
